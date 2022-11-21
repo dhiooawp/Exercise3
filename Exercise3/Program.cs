2756e6d1ca118afc96d366902ee6a6850cd4511f
DhioAWP <dhioanugerah0@gmail.com>
2022-11-21 16:08:10 +0700

    add case 1 in public void main

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -68,7 +68,32 @@ namespace Exercise_Linked_List_D
         static void Main(string[] args)
         {
             CircularList obj = new CircularList();
-            while
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. View all the records in the list");
+                    Console.WriteLine("2. Search for a record in the list");
+                    Console.WriteLine("3. Display the first record in the list");
+                    Console.WriteLine("4. Exit");
+                    Console.Write("\nEnter your choice (1-4): ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.traverse;
+                            }
+                            break;
+                        case '2':
+                            {
+
+                            }
+                    }
+
+                }
+            }
         }
     }  
 }
