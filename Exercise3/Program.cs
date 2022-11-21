238afffda9804baf80f32bafa9102671518b3f40
DhioAWP <dhioanugerah0@gmail.com>
2022-11-21 15:36:35 +0700

    Add project files.

---
+++ Exercise3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Linked_List_A
+{
+    class node
+    {
+        /*creates Nodes for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+        public node 
+    }
+}
